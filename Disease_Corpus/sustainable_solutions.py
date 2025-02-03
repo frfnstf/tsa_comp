e3a7ddfe110ce783aa4f9a9c1fc260ee81c6667f
frfnstf <106760617+frfnstf@users.noreply.github.com>
2025-02-03 01:34:03 -0600

    Add files via upload

---
+++ Disease_Corpus/sustainable_solutions.py
@@ -0,0 +1,24 @@
+# File containing a dictionary of the sustainable solutions
+# Maps keywords / key phrases to sentences about their sustainability
+
+solutions_dict = {
+    "fungicide": "Instead of fungicides, use baking soda or vinegar to avoid killing beneficial soil microbes.",
+    "fertilizer": "Use organic fertilizer instead of synthetic fertilizer to prevent releasing greenhouse gas emissions.",
+    "composting": "Composting diverts organic waste from landfills and improves soil quality.",
+    "alcohol": "Refrain from using rubbing alcohol to clean tools.",
+    "thinning": "Implement the practice of thinning your trees in order to limit competition for resources and reduce the risk of fires.",
+    "pruning": "Prune your trees and bushes to increase soil fertility, balance the root systems, and prevent the spread of diseases.",
+    "bleach": "Use white vinegar or lemon juice to clean agricultural tools instead of bleach, which can corrode metal and damage plant tissues if residues remain.",
+    "pathogen-free seeds": "Plant pathogen-free seeds to reduce pest dissemination because healthy seeds grow into healthy plants.",
+    "mulching": "Avoid using an excessive amount of mulch to prevent suffocating the roots and causing oxygen deprivation.",
+    "organic vegetable production": "Consider planting organic vegetables instead of conventional ones to improve soil health and reduce pollution.",
+    "cultivar": "Utilize cultivars to reduce reliance on chemicals and maximize crop yields.",
+    "white latex paint": "Instead of white latex paint, look for a paint with an organic base that can be applied to tree bark for protection from insects and sunburn.",
+    "herbicide": "Instead of herbicides, use integrated weed management methods or vinegar and hot water to kill weeds without environmental contamination.",
+    "crop rotation": "Rotate what crops are grown in a certain location to improve soil health and reduce reliance on chemical inputs.",
+    "irrigation": "Irrigation itself isn't inherently sustainable, but drip irrigation and smart water management systems effectively minimize water loss.",
+    "bactericide": "Refrain from using bactericides and instead use bacteriocins or plant extracts to reduce antimicrobial resistance and toxicity.",
+    "insecticide": "Use diatomaceous earth or neem oil instead of insecticides to control insects without contaminating soil or water.",
+    "tillage": "Implement conservation tillage instead of conventional tillage to prevent soil erosion and nutrient loss.",
+    "hybrid": "Plant and breed hybrid seeds to maximize crop yields while reducing usage of water and chemical inputs.",
+}
